17cc6c34505a328b41077698b18ab08f7b076366
jeff0518 <cycivs07@icloud.com>
2022-11-04 18:26:37 +0800

    fix:修改檔案名稱 cart改成Cart Jeff 2022.11.4

---
+++ src/components/Main/Cart/Cart.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Product from "./Product";
+import styled from "styled-components";
+import Destroy from "../../icons/Destroy.svg";
+import Straight from "../../icons/Straight.svg";
+
+const SectionCart = styled.section`
+  width: 440px;
+  height: 492px;
+  background: #ffffff;
+  border: 1px solid #f0f0f5;
+  border-radius: 8px;
+  margin-top: 86px;
+  padding: 32px 24px;
+
+  .cart-info {
+    width: 392px;
+    height: 37px;
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    border-top: 1px solid var(--undone-progress-border);
+    margin-top: 32px;
+  }
+`;
+
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "破壞補丁修身牛仔褲",
+    price: 3999,
+    image: Destroy,
+  },
+  {
+    id: 2,
+    name: "刷色直筒牛仔褲",
+    price: 1299,
+    image: Straight,
+  },
+];
+
+const Cart = () => {
+  // 為了計算價錢
+  let count = 0
+  return (
+    <SectionCart>
+      <h3 className="cart-title">購物籃</h3>
+      <section>
+        {/* 因為格式都一樣只有文字跟圖片不同，所以用渲染的方式 */}
+        {PRODUCTS.map((e) => {
+          count += e.price
+          return (
+            <Product key={e.id} price={e.price} image={e.image} name={e.name} />
+          );
+        })}
+      </section>
+      <section className="cart-info shipping">
+        <div className="text">運費</div>
+        <div className="price">免費</div>
+      </section>
+      <section className="cart-info total">
+        <div className="text">小計</div>
+        <div className="price">${count}</div>
+      </section>
+    </SectionCart>
+  );
+}
+
+export default Cart
---
+++ src/components/Main/Cart/Product.js
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+import IconPlus from '../../icons/IconPlus.svg'
+import IconMinus from '../../icons/IconMinus.svg'
+import '../../../sass/BaseStyled.css'
+
+const ProductContainer = styled.div`
+  width: 392px;
+  height: 102px;
+  display: flex;
+  flex-direction: row;
+  margin-top: 34px;
+
+  .img-container {
+    margin-right: 21px;
+    width: 100px;
+    height: 100px;
+  }
+
+  .product-info {
+    display: flex;
+    flex-direction: column;
+    margin-right: 61px;
+
+    .product-name {
+      width: 158px;
+      height: 27px;
+      margin-bottom: 18px;
+      color: var(--main-text);
+    }
+
+    .product-control {
+      width: 116px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .product-count {
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 17px;
+        text-align: center;
+      }
+    }
+  }
+
+  .price {
+    /* width: 52px;
+    height: 22px; */
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 22px;
+    text-align: right;
+    color: var(--product-action-text);
+  }
+`;
+
+
+const Product = (props) => {
+  return (
+    <ProductContainer data-count="0" data-price="3999">
+      <img className="img-container" src={props.image} />
+      <div className="product-info">
+        <div className="product-name">{props.name}</div>
+        {/* <div className="product-control-container"> */}
+        <div className="product-control">
+          <a className="product-action" href="#">
+            <img src={IconMinus} alt="" className="minus" />
+          </a>
+          <span className="product-count">1</span>
+          <a className="product-action" href="#">
+            <img src={IconPlus} alt="" className="plus" />
+          </a>
+        </div>
+        {/* </div> */}
+      </div>
+      <div className="price">{props.price}</div>
+    </ProductContainer>
+  );
+}
+
+export default Product
